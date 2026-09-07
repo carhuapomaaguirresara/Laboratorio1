49401fe9f314780cd4a324fc1956ff2a8f2bdc48
Sara <carhuapoma.aguirresara2020@gmail.com>
2026-09-06 22:38:55 -0500

    Tercer commit

---
+++ Saludo.py
@@ -0,0 +1,10 @@
+respuesta = "si"
+while respuesta == "si": 
+    apellido = input("¿Cuál es tu apellido?: ")
+    if    apellido == "Carhuapoma":
+      print("¡Hola Carhuapoma! Bienvenida ")
+    else:
+      print("Hola desconocido")
+    respuesta =input("Deseas repetir saludo (si/no)" )
+print ("hasta luego")
+
